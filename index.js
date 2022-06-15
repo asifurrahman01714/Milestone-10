35f0748156383b94312d3289b24a40a5f7571455
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-06-15 22:39:28 +0600

    created an user array

--- index.js
+++ index.js
@@ -13,5 +13,16 @@ app.get('/', function (req, res) {
 app.get('/fruits/banana', (req, res)=>{
     res.send({product:'banana', quantity:100, price:1000})
 })
+
+const users = [
+    {name:"Asif", roll:10},
+    {name:"sakib", roll:11},
+    {name:"azad", roll:12},
+    {name:"rhidwan", roll:13}
+]
+app.get('/users/:id',(req,res)=>{
+    console.log(req.params);
+    res.send(req.params.id)
+})
   
 app.listen(3000, ()=> console.log("Yay! Listening to port 3000"))
