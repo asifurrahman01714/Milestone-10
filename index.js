cd7735d01cddcdc36499bdad5ef14c4bda464f02
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-06-15 21:18:42 +0600

    gitignore

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 const express = require('express')
 const app = express()
 
+app.get('/', function (req, res) {
+    res.send('Thank you for calling me')
+})
+  
+app.listen(3000, ()=> console.log("Listening to port 3000"))
