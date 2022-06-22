292f9ecafcf52087c5cdb720f6c53a14d26154b8
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-06-22 07:40:40 +0600

    Book.js in private route

---
+++ burj-al-arab/src/components/Login/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB9chOmPMzVriDxzmPq4lEzlkRhYsk5vew",
+    authDomain: "burj-al-arab-bafe3.firebaseapp.com",
+    projectId: "burj-al-arab-bafe3",
+    storageBucket: "burj-al-arab-bafe3.appspot.com",
+    messagingSenderId: "624726565791",
+    appId: "1:624726565791:web:9445db094bc8ad0b1a13aa"
+  };
+
+export default firebaseConfig;
