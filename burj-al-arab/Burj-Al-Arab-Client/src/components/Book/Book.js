5c0285057ba9a48d49c0d49bc253d95b9986a65f
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-06-23 09:45:17 +0600

    material ui date range problem solved

---
+++ burj-al-arab/Burj-Al-Arab-Client/src/components/Book/Book.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import Box from '@mui/material/Box';
+
+const Book = () => {
+    const {bedType} = useParams();
+    const [value, setValue] = React.useState([null, null]);
+    return (
+        
+            <LocalizationProvider
+                dateAdapter={AdapterDateFns}
+                localeText={{ start: 'Check-in', end: 'Check-out' }}
+                >
+                <DateRangePicker
+                    value={value}
+                    onChange={(newValue) => {
+                    setValue(newValue);
+                    }}
+                    renderInput={(startProps, endProps) => (
+                    <React.Fragment>
+                        <TextField {...startProps} />
+                        <Box sx={{ mx: 2 }}> to </Box>
+                        <TextField {...endProps} />
+                    </React.Fragment>
+                    )}
+                />
+            </LocalizationProvider>
+       
+    );
+};
+
+export default Book;
